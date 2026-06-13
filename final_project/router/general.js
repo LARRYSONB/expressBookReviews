76cc0ced086aa2fc316b5a5a1ae492d75cd9197c
LARRYSONB <133322555+LARRYSONB@users.noreply.github.com>
2026-06-12 22:24:31 -0400

    Simplify author book retrieval with filter
    Refactored author search logic to use filter method.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,23 +32,9 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const author = req.params.author
-  const keys = Object.keys(books);
-  const matchingBooks = [];
-
-  // Iterate through the keys to check the author
-  keys.forEach(key => {
-    if (books[key].author.toLowerCase() === author) {
-      matchingBooks.push({ isbn: key, ...books[key] });
-    }
-  });
-
-  if (matchingBooks.length > 0) {
-    return res.status(200).json(matchingBooks);
-  } else {
-    return res.status(404).json({ message: "No books found by this author" });
-  }
-
+  const author = req.params.author;
+  let filtered_book = books.filter((books) => book.author === author);
+  res.send(filtered_books);
 });
 
 // Get all books based on title
