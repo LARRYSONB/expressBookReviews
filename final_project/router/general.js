5f0b909f1170e8541c77cf664303ccaf1754b5b1
LARRYSONB <133322555+LARRYSONB@users.noreply.github.com>
2026-06-12 21:39:10 -0400

    Add endpoint to get books by author
    Implemented functionality to retrieve books by author.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,7 +32,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  const keys = Object.keys(books);
+  const matchingBooks = [];
+
+  // Iterate through the keys to check the author
+  keys.forEach(key => {
+    if (books[key].author.toLowerCase() === author) {
+      matchingBooks.push({ isbn: key, ...books[key] });
+    }
+  });
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).json(matchingBooks);
+  } else {
+    return res.status(404).json({ message: "No books found by this author" });
+  }
+
 });
 
 // Get all books based on title
